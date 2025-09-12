734c84a38f10ecc002b23629d3480a803bae50c9
Akif Bhuiyan <bhuiyaakif@gmail.com>
2025-09-12 15:19:37 -0400

    Completed 4.1

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,22 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int n = 1000;
+        int a = 3;
+        int b = 5;
+
+        System.out.println(multiples(n, a, b));
+    }
+
+    public static int multiples(int n, int a, int b) {
         int x = 1;
         int count = 0;
-        while (x < 1000) {
-            if (x % 3 == 0 || x % 5 == 0) {
+        while (x < n) {
+            if (x % a == 0 || x % b == 0) {
                 count++;
             }
             x++;
         }
-        System.out.println(count);
+        return count;
     }
 }
